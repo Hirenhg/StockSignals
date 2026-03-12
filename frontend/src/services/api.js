e70a7afac86d11fa6b327bfa9d69c492ddf37684
Hirenhg <83075919+Hirenhg@users.noreply.github.com>
2026-03-12 15:51:47 +0530

    fix: api change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000" || "https://stocksignals-65rz.onrender.com",
+  // baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000" || "https://stocksignals-65rz.onrender.com",
+  baseURL: "https://stocksignals-65rz.onrender.com",
   timeout: 30000,
   headers: {
     'Cache-Control': 'no-cache',
