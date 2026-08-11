24495a88c083f64a5fad9deba642b3302f26ba9b
Hiren Gabu <hiren.gabu@ics-global.in>
2026-08-11 12:21:29 +0530

    index levals page update

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
   const navItems = isLoggedIn
     ? [
         { path: '/', label: 'Dashboard', icon: 'bi-arrows-expand' },
-        // { path: '/index-levels', label: 'Index Levels', icon: 'bi-bar-chart-steps' },
+        { path: '/index-levels', label: 'Index Levels', icon: 'bi-bar-chart-steps' },
         { path: '/signals', label: 'Signals', icon: 'bi-speedometer2' },
         // { path: '/market-depth', label: 'Market Depth', icon: 'bi-people-fill' },
         // { path: '/options', label: t('options'), icon: 'bi-gear' },
